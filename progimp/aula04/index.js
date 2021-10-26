5970b55d1eaf7afa8b135f4e4a4c670a25a4e16c
yasmink88 <yasmin.khalaf.a@gmail.com>
2021-10-26 19:37:39 -0300

    Initial upload

---
+++ progimp/aula04/index.js
@@ -0,0 +1,29 @@
+/*console.log("Hello World!");
+
+var x = 8;
+const z = 25;
+
+let y = 5;
+
+let nome = "Yasmin"
+let plano = true;
+let soma = x + z;
+let sub = z - y;
+let mult = y * x;
+let div = z / y;
+
+console.log( nome, plano, soma, sub, mult, div );
+*/
+
+//IMC
+
+let nome, idade, peso, altura, plano, imc
+
+nome = "Carlos de Souza"
+idade = 28
+peso = 80.1
+altura = 1.76
+plano = true
+imc = peso / (altura * altura)
+
+console.log(`${nome} tem ${idade} anos e seu índice de massa corporal é de ${imc}`)

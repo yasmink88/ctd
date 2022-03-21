9e6c81e6a3dce70b62fda343f8b21963bdf4fea6
yasmink88 <yasmin.khalaf.a@gmail.com>
2022-03-21 19:20:53 -0300

    updating checkpoint

--- frontend/2/aula09/calculator.js
+++ frontend/2/aula09/calculator.js
@@ -1,18 +1,18 @@
 //saving the HTML elements we need in variables
 const calculator = document.querySelector(".calculator");
-const display = document.querySelector(".display")
-const keys = document.querySelector(".keys");
+const display = calculator.querySelector(".display")
+const keys = calculator.querySelector(".keys");
 
 //adding the event listener to the buttons
 keys.addEventListener("click", (element) => {
 
     
   if (element.target.matches("button")) {
-    const key = element.target; //getting each button
-    const action = key.dataset.action; //getting the action (HTML attribute data-action) of the buttons
-    const keyContent = key.textContent //getting the content of each button
-    let displayContent = display.textContent //getting the content of the display
     
+    const key = element.target; //getting each button
+    const keyContent = key.textContent //getting the content of each button
+    const action = key.dataset.action; //getting the action (HTML attribute data-action) of the buttons
+    let displayContent = display.textContent //getting the content of the display
     
     //determining which button is clicked
     if (!action) {
@@ -21,6 +21,7 @@ keys.addEventListener("click", (element) => {
         } else {
             display.textContent = displayContent + keyContent
         }
+
     } 
     
     else if (
@@ -29,35 +30,25 @@ keys.addEventListener("click", (element) => {
       action === "multiply" ||
       action === "divide"
     ) {
-        switch (action) {
-            case "add": 
-            if(!displayContent.includes('+')) {
-                display.textContent = displayContent + '+'
-            }
+
+
+       /*  switch (action) {
+            case "add":  display.textContent = displayContent + '+'
             
+                break;
+            case "subtract": display.textContent = displayContent + '-'
                 
                 break;
-            case "subtract": if(!displayContent.includes('-')) {
-                display.textContent = displayContent + '-'
-            }
+            case "multiply": display.textContent = displayContent + '*'
                 
                 break;
-            case "multiply": if(!displayContent.includes('*')) {
-                display.textContent = displayContent + '*'
-            }
+            case "divide": display.textContent = displayContent + '/'
                 
                 break;
-            case "divide": if(!displayContent.includes('/')) {
-                display.textContent = displayContent + '/'
-            }
-                
-                break;
-        
             default:
                 break;
-        }
+        } */
         console.log("You clicked on an operator key")
-        key.classList.add('operatorPressed')
 
     }
 
--- frontend/2/checkpoint-i/app.js
+++ frontend/2/checkpoint-i/app.js
@@ -1,7 +1,8 @@
 const botaoEnviar = document.getElementById('botaoEnviar');
+let card = document.querySelector('.cards')
 
 
-console.log(newCard)
+
 function executar() {
     //grabbing form info
     let formUrl = document.getElementById('url').value
@@ -10,13 +11,16 @@ function executar() {
 
     //adding from values to cards
     let newContent = `
+    <div class="card">
     <img src="${formUrl}" alt="">
     <h2>${formPais}</h2>
-    <p>${formDescricao}</p>`
+    <p>${formDescricao}</p>
+    </div>`
 
+    card.innerHTML = card.innerHTML + newContent
 }
 
 botaoEnviar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    executar() 
+    executar()
 })

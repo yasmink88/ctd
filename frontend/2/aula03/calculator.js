83766e6195f9382a1d2f3fac212396ee4dd0780d
yasmink88 <yasmin.khalaf.a@gmail.com>
2022-03-28 14:05:52 -0300

    updating classes

---
+++ frontend/2/aula03/calculator.js
@@ -0,0 +1,10 @@
+const d = require("./divide.js")
+const m = require("./multiply.js")
+const a = require("./add.js")
+const s = require("./subtract.js")
+
+
+console.log(d.divide(2, 3))
+console.log(m.multiply(2, 3))
+console.log(a.add(2, 3))
+console.log(s.subtract(2, 3))

63661e3fe7ebf4d4c44b1a7ecb36ef1b63836b02
yasmink88 <yasmin.khalaf.a@gmail.com>
2022-03-29 22:21:48 -0300

    updating python

--- infraestrutura/1/aula08/game.py
+++ infraestrutura/1/aula08/game.py
@@ -1,5 +1,18 @@
 import random
 
+userScore = 0
+pcScore = 0
+totalScore = 0
+
+def percent():
+        if totalScore > 0: 
+            x = ((totalScore - pcScore) / totalScore) * 100
+            return x
+        elif totalScore == 0:
+            x = 0
+            return x
+
+
 while True: 
     aleatorio = random.randrange(0, 5)
     escolhaPc = ""
@@ -8,7 +21,8 @@ while True:
     print("3)Pedra")
     print("4)Lagarto")
     print("5)Spock")
-    print("6)Sair do Programa")
+    print("6) Mostrar pontuação")
+    print("7)Sair do Programa")
     opcao = int(input("O que você escolhe: "))
     
     if opcao == 1:
@@ -17,11 +31,18 @@ while True:
         escolhaUsuario = "papel"
     elif opcao == 3:
         escolhaUsuario = "pedra"
-    elif opcao == 3:
+    elif opcao == 4:
         escolhaUsuario = "lagarto"
-    elif opcao == 3:
+    elif opcao == 5:
         escolhaUsuario = "spock"
     elif opcao == 6:
+        print ("Pontuações: ")
+        print ("Usuário: ", userScore)
+        print ("Pc: ", pcScore)
+        print ("Porcentagem de vitórias: ", percent(), "% " )
+        continue
+
+    elif opcao == 7:
         print ("Nos vemos!")
         break
     else:
@@ -44,48 +65,68 @@ while True:
     
     if escolhaPc == "pedra" and escolhaUsuario == "papel":
         print("Ganhou, papel cobre pedra")
+        userScore += 1
     elif escolhaPc == "spock" and escolhaUsuario == "papel":
         print("Ganhou, papel refuta spock")
+        userScore += 1
     elif escolhaPc == "papel" and escolhaUsuario == "tesoura":
         print("Ganhou, tesoura corta papel")
+        userScore += 1
     elif escolhaPc == "lagarto" and escolhaUsuario == "tesoura":
         print("Ganhou, tesoura decapita lagarto")
+        userScore += 1
     elif escolhaPc == "tesoura" and escolhaUsuario == "pedra":
         print("Ganhou, pedra amassa tesoura")
+        userScore += 1
     elif escolhaPc == "lagarto" and escolhaUsuario == "pedra":
         print("Ganhou, pedra esmaga lagarto")
+        userScore += 1
     elif escolhaPc == "tesoura" and escolhaUsuario == "spock":
         print("Ganhou, spock quebra tesoura")
+        userScore += 1
     elif escolhaPc == "pedra" and escolhaUsuario == "spock":
         print("Ganhou, spock vaporiza pedra")
+        userScore += 1
     elif escolhaPc == "papel" and escolhaUsuario == "lagarto":
         print("Ganhou, lagarto come papel")
+        userScore += 1
     elif escolhaPc == "spock" and escolhaUsuario == "lagarto":
         print("Ganhou, lagarto envenena spock")
+        userScore += 1
 
 
 
         
     if escolhaUsuario == "pedra" and escolhaPc == "papel":
         print("Perdeu, papel cobre pedra")
+        pcScore += 1
     elif escolhaUsuario == "spock" and escolhaPc == "papel":
         print("Perdeu, papel refuta spock")
+        pcScore += 1
     elif escolhaUsuario == "papel" and escolhaPc == "tesoura":
         print("Perdeu, tesoura corta papel")
+        pcScore += 1
     elif escolhaUsuario == "lagarto" and escolhaPc == "tesoura":
         print("Perdeu, tesoura decapita lagarto")
+        pcScore += 1
     elif escolhaUsuario == "tesoura" and escolhaPc == "pedra":
         print("Perdeu, pedra amassa tesoura")
+        pcScore += 1
     elif escolhaUsuario == "lagarto" and escolhaPc == "pedra":
         print("Perdeu, pedra esmaga lagarto")
+        pcScore += 1
     elif escolhaUsuario == "tesoura" and escolhaPc == "spock":
         print("Perdeu, spock quebra tesoura")
+        pcScore += 1
     elif escolhaUsuario == "pedra" and escolhaPc == "spock":
         print("Perdeu, spock vaporiza pedra")
+        pcScore += 1
     elif escolhaUsuario == "papel" and escolhaPc == "lagarto":
         print("Perdeu, lagarto come papel")
+        pcScore += 1
     elif escolhaUsuario == "spock" and escolhaPc == "lagarto":
         print("Perdeu, lagarto envenena spock")
+        pcScore += 1
     elif escolhaPc == escolhaUsuario:
         print("Empate")
     again = input("Jogar novamente? s/n: ")
